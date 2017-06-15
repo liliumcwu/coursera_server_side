166e74240c946dd2fa4bef188565b89798a3bd34
Lilium Wu <lilium.wu@yale.edu>
2017-06-15 09:54:13 -0700

    finishes tasks 2 and 3 (promoRouter and leaderRouter modules) of assignment 1

---
+++ node-express/promoRouter.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+// enables us to parse the data and add it to a javascript object
+
+var promoRouter = express.Router();
+promoRouter.use(bodyParser.json());
+//if json, bodyParser will parse and make available to promoRouter
+promoRouter.route('/')
+.all(function(req,res,next) {
+  res.writeHead(200, {'Content-type': 'text/plain'});
+  next();
+})
+
+.get(function(req,res,next) { // get is chained to .route
+  res.end('Will send all the promos to you!');
+}) // so no semicolon
+
+.post(function(req,res,next) { // ditto
+  res.end('Will add the promo: ' + req.body.name + ' with details: '
+          + req.body.description);
+}) // ditto
+
+.delete(function(req,res,next) { // ditto
+  res.end('Deleting all promos');
+}); // semicolon completes the chain
+
+promoRouter.route('/:promoId')
+.all(function(req,res,next) {
+  res.writeHead(200, {'Content-type': 'text/plain'});
+  next();
+})
+
+.get(function(req,res,next) {
+  res.end('Will send the details of the promo: ' + req.params.promoId
+  + ' to you!');
+})
+
+.put(function(req,res,next) {
+  res.write('Updating the promo: ' + req.params.promoId + '\n');
+  res.end('Will update the promo: ' + req.body.name + ' with details: '
+  + req.body.description);
+})
+
+.delete(function(req,res,next) {
+  res.end('Deleting promo: ' + req.params.promoId);
+}); // completes this chain
+
+module.exports = promoRouter;
--- node-express/server-5.js
+++ node-express/server-5.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var morgan = require('morgan'); // allows us to log info on server side
 var dishRouter = require('./dishRouter');
+var promoRouter = require('./promoRouter');
+var leaderRouter = require('./leaderRouter');
 
 var hostname = 'localhost';
 var port = 3000;
@@ -11,6 +13,8 @@ var app = express();
 app.use(morgan('dev')); // dev is a preprogrammed log output that morgan supports
 
 app.use('/dishes', dishRouter); // if url contains /dishes, apply dishRouter
+app.use('/promos', promoRouter); // if url contains /promos, apply promoRouter
+app.use('/leaders', leaderRouter); // if url contains /leaders, apply leaderRouter
 
 app.use(express.static(__dirname + '/public')); // public is source of static files
 
